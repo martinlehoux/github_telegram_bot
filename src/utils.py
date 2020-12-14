93f8a4144920649a8c512d88641545121bee422b
Martin Kagamino Lehoux <martin@lehoux.net>
2020-12-14 11:21:52 +0100

    [Fix] Escape has been moved to an upper level

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from src.models import Label
 def make_labels(labels: List[Label]) -> str:
     labels_str = "-".join([label.name for label in labels])
     if labels_str:
-        labels_str = r" \(" + labels_str + r"\)"
+        labels_str = f" ({labels_str})"
     return labels_str
 
 
